511de91eaedb7723c8e849ffeb69772de33dd9fd
FernandaMancini <108295414+FernandaMancini@users.noreply.github.com>
2022-10-22 13:33:02 -0300

    Update resolucao-exercicio3.js

--- resolucao-exercicio3.js
+++ resolucao-exercicio3.js
@@ -1,5 +1,7 @@
-produto = 21
-if (produto < 20)
-    console.log((produto*0.45)+produto)
-else
-    console.log((produto*0.30)+produto) 
+let produto = 21;
+if (produto < 20){
+    console.log((produto*0.45)+produto);
+}
+else{
+    console.log((produto*0.30)+produto); 
+}
